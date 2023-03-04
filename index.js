d3c923caf7a5082db66023cd9f78c269f9c591a3
abrarazmain <ajmainabrar5@gmail.com>
2023-03-04 20:06:44 +0600

    added accouracy btn

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ const fetchAiTools = () => {
 
 // function for display API data on DOM
 const displayAiTools = (data) => {
-  console.log(data);
+  console.log(data.tools);
   const container = document.getElementById("card-container");
+  const showAll = document.getElementById("btn-show-all");
   // ===============================================
-  document
-    .getElementById("btn-show-all")
-    .addEventListener("click", function () {
-      // Clear the container element
-      container.innerHTML = "";
 
-      data.tools.forEach((singleDta) => {
-        // console.log(singleDta);
-        container.innerHTML += `
+  showAll.addEventListener("click", function () {
+    // Clear the container element
+    container.innerHTML = "";
+    showAll.classList.add("d-none");
+    data.tools.forEach((singleDta) => {
+      // console.log(singleDta);
+      container.innerHTML += `
           <di v class="card col-3" style="height: 500px;">
     <img src="${singleDta.image}" class="card-img-top my-3" alt="...">
     <div class="card-body">
@@ -49,10 +49,9 @@ const displayAiTools = (data) => {
       </div>
     </div>
   </div>`;
-      });
-
-      toggleSpinner(false);
     });
+    toggleSpinner(false);
+  });
   // ===============================================
   data.tools.slice(0, 6).forEach((singleDta) => {
     // console.log(singleDta);
@@ -87,6 +86,7 @@ const displayAiTools = (data) => {
   toggleSpinner(false);
 };
 
+// function for fetch single card
 const fetchSingleCard = (id) => {
   // console.log(id);
   const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
@@ -96,14 +96,21 @@ const fetchSingleCard = (id) => {
     .then((data) => displaySingleCard(data.data));
 };
 
+// function for display single card on modal
 const displaySingleCard = (data) => {
-  // console.log(data);
-  const container = document.getElementById("modal-container");
+  console.log(data);
   document.getElementById("modal-1-title").innerText = data.description;
   document.getElementById("plan").innerHTML = `
-    <div class="text-success fw-bold bg-light p-3 rounded">${data.pricing[0].price} /<br> ${data.pricing[0].plan}</div>
-    <div class="text-danger-emphasis fw-bold bg-light p-3 rounded">${data.pricing[1].price} /<br> ${data.pricing[1].plan}</div>
-    <div class="text-warning-emphasis fw-bold bg-light p-3 rounded">${data.pricing[2].price} /<br> ${data.pricing[2].plan}</div>
+   <div class="text-success fw-bold bg-light p-3 rounded">${
+     data.pricing[0].price ? data.pricing[0].price : "no data found"
+   } 
+   <br> ${data.pricing[0].plan}</div>
+    <div class="text-danger-emphasis fw-bold bg-light p-3 rounded">${
+      data.pricing[1].price ? data.pricing[1].price : "no data found"
+    } <br> ${data.pricing[1].plan}</div>
+    <div class="text-warning-emphasis fw-bold bg-light p-3 rounded">${
+      data.pricing[2].price ? data.pricing[2].price : "no data found"
+    } <br> ${data.pricing[2].plan}</div>
     `;
   document.getElementById("card-feature").innerHTML = `
     <div>
@@ -117,21 +124,27 @@ const displaySingleCard = (data) => {
    <div>
    <h5 class="card-title">Integrations</h5>
    <ul>
-   <li>${data.integrations[0]}</li>
-   ${data.integrations[1] ? `<li>${data.integrations[1]}</li>` : ""}
-   ${data.integrations[2] ? `<li>${data.integrations[2]}</li>` : ""}
+   ${data.integrations ? `<li>${data.integrations[0]}</li>` : ""}
+   ${data.integrations ? `<li>${data.integrations[1]}</li>` : ""}
+   ${data.integrations ? `<li>${data.integrations[2]}</li>` : ""}
    </ul> 
    </div>
     `;
   // modal card 2
   document.getElementById("modal2").innerHTML = `
+    <div class="test">
     <img src="${data.image_link[0]}" class="card-img-top" alt="...">
+    <button id="special-btn type="button" class="${data.accuracy.score == null ? 'd-none' : 'd-block'} fw-bold test2 btn btn-primary"
+        style="--bs-btn-padding-y: .35rem; --bs-btn-padding-x: .30rem; --bs-btn-font-size: .80rem;">
+  ${data.accuracy.score}% Accuracy
+</button>
+    </div>
     <h5 class="card-title d-flex justify-content-center">${data.input_output_examples[0].input}</h5>
     <p class="d-flex justify-content-center mx-auto">${data.input_output_examples[0].output}</p>
     `;
 };
 
-// loader function
+// function for loader
 const toggleSpinner = (isLoading) => {
   const spinner = document.getElementById("spinner");
   if (isLoading) {
@@ -141,18 +154,57 @@ const toggleSpinner = (isLoading) => {
   }
 };
 
-// function for getting data from APi
-const fetchAiToolsForseeMore = () => {
-  const URL = `https://openapi.programming-hero.com/api/ai/tools`;
-  fetch(URL)
+// =================================
+const fetchApiDate = () => {
+  const apiUrl = "https://openapi.programming-hero.com/api/ai/tools";
+
+  fetch(apiUrl)
     .then((res) => res.json())
-    .then((data) => {
-      displayApiForSeeMore(data);
-    })
-    .catch((error) => console.error(error));
+    .then((data) => displayCardsByDate(data.data));
+};
+// ====================================
+const displayCardsByDate = (data) => {
+  const container = document.getElementById("card-container");
+
+  // Sort the data by date in descending order
+  const sortedData = data.sort(
+    (a, b) => new Date(b.published_in) - new Date(a.published_in)
+  );
+
+  // Display the cards in the container
+  sortedData.forEach((singleData) => {
+    container.innerHTML += `
+      <div class="card col-3" style="height: 500px;">
+        <img src="${singleData.image}" class="card-img-top my-3" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Features</h5>
+          <ol>
+            ${
+              singleData ? `<li>${singleData.features[0]}</li>` : ""
+            }
+            ${
+              singleData ? `<li>${singleData.features[1]}</li>` : ""
+            }
+            ${
+              singleData ? `<li>${singleData.features[2]}</li>` : ""
+            }
+          </ol>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <h5 class="card-title">${singleData.name}</h5>
+              <p class="card-text"><small class="text-muted"><i class="fa-solid fa-calendar-days"></i> ${
+                singleData.published_in
+              }</small></p>
+            </div>
+            <div>
+              <p data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="fetchSingleCard('${
+                singleData.id
+              }')" class="btn btn-outline-secondary bg-light-subtle"><i class="fa-solid fa-arrow-right"></i></p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  });
 };
 
-const displayApiForSeeMore = (data) => {
-  console.log(data);
-};
-// fetchAiTools()
